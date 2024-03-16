7b3ec63ed37af55299617a4a5b079ca3df3bdf1d
Awwab <mawwabkhank2006@gmail.com>
2024-03-16 11:12:57 +0500

    question 1 solved

---
+++ Big O Notation/interview_questions_bigO.py
@@ -0,0 +1,13 @@
+################ Interview Questions #############
+#Question1 - O(n)
+def foo(array):
+    sum = 0
+    product = 1
+    for i in array:
+        sum += i
+    for i in array:
+        product *= i
+    print("Sum = "+str(sum)+", Product = "+str(product))
+
+ar1 = [1,2,3,4]
+foo(ar1)
