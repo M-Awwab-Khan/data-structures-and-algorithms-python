20692aab3a75f87141c87d8c5a37214dffea8ab6
Awwab <mawwabkhank2006@gmail.com>
2024-03-16 11:38:37 +0500

    question 10 O(log n)

--- Big O Notation/interview_questions_bigO.py
+++ Big O Notation/interview_questions_bigO.py
@@ -83,3 +83,21 @@ def fib(n):
 
 
 allFib(4)
+
+#Question10 - o(log n)
+def powersOf2(n):
+    # print("n:"+str(n))
+    if n < 1:
+        return 0
+    elif n == 1:
+        print(1)
+        return 1
+    else:
+        prev = powersOf2(int(n/2))
+        # print("prev:"+str(prev))
+        print(prev)
+        curr = prev*2
+        print(curr)
+        return curr
+
+powersOf2(50)
