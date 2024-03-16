c35d61d463b9174ce9967cbef402bdfd15a52aaf
Awwab <mawwabkhank2006@gmail.com>
2024-03-16 11:01:49 +0500

    recursive vs. iterative time complexity

--- Big O Notation/time_complexities.py
+++ Big O Notation/time_complexities.py
@@ -45,3 +45,35 @@ for b in arrayB:
 for a in arrayA:
     for b in arrayB:
         print(a,b)
+
+######  Iterative algorithm - finding the biggest number in the array ####### 
+
+sample1Array = [1,10,45,33,23,45,67,2,3,33,55,11,65,76,34,35,27,99]
+
+def findBiggestNumber(sampleArray):
+    biggestNumber = sampleArray[0]
+    for index in range(1,len(sampleArray)):
+        if sampleArray[index] > biggestNumber:
+            biggestNumber = sampleArray[index]
+    print(biggestNumber)
+
+findBiggestNumber(sample1Array)
+
+######  Recursive algorithm - finding the biggest number in the array ####### 
+
+def findMaxNumRec(sampleArray, n):
+    if n == 1:
+       return sampleArray[0]
+    return max(sampleArray[n-1],findMaxNumRec(sampleArray,n-1))
+
+print(findMaxNumRec(sample1Array,len(sample1Array)))
+
+
+######  Recursive algorithm multiple calls ####### 
+
+def f(n):
+    if n <= 1:
+        return 1
+    return f(n-1) + f(n-1)
+
+print(f(3))
