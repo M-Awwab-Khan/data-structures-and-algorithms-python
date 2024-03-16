44ba743cc1351a48a638530687ed64f8dee5002d
Awwab <mawwabkhank2006@gmail.com>
2024-03-16 11:31:23 +0500

    question 8 O(n)

--- Big O Notation/interview_questions_bigO.py
+++ Big O Notation/interview_questions_bigO.py
@@ -57,3 +57,14 @@ def reverse(array):
     print(array)
 
 reverse(arrayA)
+
+#Question8 - O(n)
+def factorial(n):
+    if n < 0:
+        return -1
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(3))
