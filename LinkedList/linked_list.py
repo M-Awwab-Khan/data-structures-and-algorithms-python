009d35ec2444c687cb06a8fbf8b621875cf2ded6
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-04 19:56:00 +0500

    linked list init, len, append implemented

---
+++ LinkedList/linked_list.py
@@ -0,0 +1,22 @@
+from node import Node   
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        self.tail = self.head
+
+    def __len__(self) --> int:
+        return self.size
+
+    def append(self, node: Node) -> None:
+        if self.head:
+            self.tail.next = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.head = node
+            self.tail = self.head
+            self.size = 1
+
+
