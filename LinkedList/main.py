caf0565801f541a8ca9328e51bf4adf19a688da8
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-06 21:25:09 +0500

    testing linked list

---
+++ LinkedList/main.py
@@ -0,0 +1,13 @@
+from linked_list import LinkedList
+
+lst = LinkedList()
+lst.append(1)
+lst.append(2)
+lst.append(3)
+lst.append(4)
+lst.append(5)
+lst.append(6)
+
+lst.prepend(0)
+print(lst)
+print(len(lst))
