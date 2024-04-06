281ccfff431f619f9c2252aa5fca54289feec784
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-06 21:25:25 +0500

    bugs fixed

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -6,35 +6,38 @@ class LinkedList:
         self.size = 0
         self.tail = self.head
 
-    def __len__(self) --> int:
+    def __len__(self) -> int:
         return self.size
 
-    def append(self, node: Node) -> None:
+    def append(self, data) -> None:
+        new_node = Node(data)
         if self.head:
-            self.tail.next = node
-            self.tail = node
+            self.tail.next = new_node
+            self.tail = new_node
             self.size += 1
         else:
-            self.head = node
+            self.head = new_node
             self.tail = self.head
             self.size = 1
 
-    def prepend(self, node: Node) -> None:
+    def prepend(self, data) -> None:
+        new_node = Node(data)
         if self.head:
-            node.next = self.head
-            self.head = node
+            new_node.next = self.head
+            self.head = new_node
             self.size += 1
         else:
-            self.head = node
+            self.head = new_node
             self.tail = self.head
             self.length = 1
 
     def __str__(self):
-        print('LinkedList([', end='')
+        string = 'LinkedList(['
         if self.head:
             current_node = self.head
             while current_node:
-                print(current_node.data, end=', ')
+                string += f"{current_node.value}, "
                 current_node = current_node.next
 
-        print('])')
+        string += '])'
+        return string
