ba22862d6c4c12eab858c687860acc373b2d1b2f
Awwab <mawwabkhank2006@gmail.com>
2024-04-13 10:00:05 +0500

    testing

--- LinkedList/main.py
+++ LinkedList/main.py
@@ -8,25 +8,29 @@ lst.append(4)
 lst.append(5)
 lst.append(6)
 
-lst.prepend(0)
+# lst.prepend(0)
+# print(lst)
+# print(len(lst))
+
+# lst.insert(3, -1)
+# print(lst)
+
+# lst.delete_by_value(4)
+# print(lst)
+
+# lst.delete_by_value(6)
+# print(lst)
+
+# lst.delete_by_value(0)
+# print(lst)
+
+# lst.delete_by_position(0)
+# print(lst)
+
+# lst.delete_by_position(1)
 print(lst)
 print(len(lst))
 
-lst.insert(3, -1)
-print(lst)
-
-lst.delete_by_value(4)
-print(lst)
-
-lst.delete_by_value(6)
-print(lst)
-
-lst.delete_by_value(0)
-print(lst)
-
-lst.delete_by_position(0)
-print(lst)
-
-lst.delete_by_position(1)
+lst.reverse()
 print(lst)
 print(len(lst))
