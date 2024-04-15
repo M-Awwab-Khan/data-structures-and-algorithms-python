562098a0723c14d84978cfe41550ce1c29615f19
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-15 20:19:18 +0500

    testing

---
+++ Stacks/main.py
@@ -0,0 +1,16 @@
+from stack_using_array import Stack
+
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+print(stack)
+stack.pop()
+
+print(stack)
+print(stack.peak())
+
