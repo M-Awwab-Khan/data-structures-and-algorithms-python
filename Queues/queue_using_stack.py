5b29060602d9d59ee4f10b538d45d881443430ce
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-16 23:07:57 +0500

    enqueue implementation using two stacks

--- Queues/queue_using_stack.py
+++ Queues/queue_using_stack.py
@@ -13,4 +13,12 @@
 class Queue:
 
     def __init__(self):
-        ...
+        self.s1 = []
+        self.s2 = []
+
+    def enqueue(self, data):
+        for i in range(len(self.s1)):
+            self.s2.append(self.s1.pop())
+        self.s1.append(data)
+        for i in range(len(self.s2)):
+            self.s1.append(self.s2.pop())
