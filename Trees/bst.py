11e51c661eedb65a4de4eb8996a441b4988f2502
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-21 11:21:59 +0500

    insert method implementation

--- Trees/bst.py
+++ Trees/bst.py
@@ -1,7 +1,33 @@
+from node import Node
+
 class BinarySearchTree:
 
     def __init__(self):
         self.root = None
         self.number_of_nodes = 0
+
+    def insert(self, data):
+
+        new_node = Node(data)
+        if self.root is None:
+            self.root = new_node
+            self.number_of_nodes += 1
+
+        else:
+            current_node = self.root
+            while (current_node.left != new_node) and (current_node.right != new_node):
+                if new_node.data > current_node.data:
+                    if current_node.right is None:
+                        current_node.right = new_node
+                    else:
+                        current_node = current_node.right
+                elif new_node.data < current_node.data:
+                    if current_node.left is None:
+                        current_node.left = new_node
+                    else:
+                        current_node = current_node.left
+            self.number_of_nodes += 1
+            return
+
 
 
