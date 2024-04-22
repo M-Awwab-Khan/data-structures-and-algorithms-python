97e10dbbbad728f03c99a5dbdd4e9d581a8c561a
Awwab <mawwabkhank2006@gmail.com>
2024-04-22 19:33:02 +0500

    remove duplicates without using extra space

--- Interview Qs LinkedList/removedups.py
+++ Interview Qs LinkedList/removedups.py
@@ -13,7 +13,21 @@ def removeDups(ll):
                 visited.add(currentNode.next.value)
                 currentNode = currentNode.next
         return ll
-        
+    
+def removeDups1(ll):
+    if ll.head is None:
+        return
+    
+    currentNode = ll.head
+    while currentNode:
+        runner = currentNode
+        while runner.next:
+            if runner.next.value == currentNode.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        currentNode = currentNode.next
+    return ll.head
     
 customLL = LinkedList()
 customLL.generate(10, 0, 99)
